0787926176eb1a6d994eaddf871096d8e8445404
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-04-09 11:36:39 +0800

    Unity_幼獅_課後練習1
    新增 Enemy.cs、EnemyManager.cs
    完成推送至GitHub

---
+++ ClassField/Assets/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    /// <summary>
+    /// 血量
+    /// </summary>
+    [Header("血量"),Tooltip("怪物的血量"),Range(50,500)]
+    public int hp = 200;
+    /// <summary>
+    /// 魔力
+    /// </summary>
+    [Header("魔力"), Tooltip("怪物的魔力"),Range(10,100)]
+    public int mp = 50;
+    /// <summary>
+    /// 移動速度
+    /// </summary>
+    [Header("移動速度"),Range(1f,50.5f)]
+    public float speed = 10.5f;
+    /// <summary>
+    /// 攻擊力
+    /// </summary>
+    [Header("攻擊力"),Range(20,200)]
+    public int attack = 50;
+    /// <summary>
+    /// 防禦力
+    /// </summary>
+    [Header("防禦力"),Range(0,100)]
+    public int defense = 30;
+    /// <summary>
+    /// 武器名稱
+    /// </summary>
+    [Header("武器名稱")]
+    public string weapon = "噬血小刀";
+    /// <summary>
+    /// 裝備名稱
+    /// </summary>
+    [Header("裝備名稱")]
+    public string equipment = "皮衣";
+    /// <summary>
+    /// 是否帶有鑰匙
+    /// </summary>
+    [Header("是否帶有鑰匙"), Tooltip("怪物是否帶有鑰匙")]
+    public bool key = false;
+    /// <summary>
+    /// 是否掉落寶物
+    /// </summary>
+    [Header("是否帶有寶物"), Tooltip("怪物是否帶有寶物")]
+    public bool treasure = false;
+}
---
+++ ClassField/Assets/EnemyManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    /// <summary>
+    /// 定義怪物
+    /// </summary>
+    [Header("怪物 1 號")]
+    public Enemy enemy1;
+    [Header("怪物 2 號")]
+    public Enemy enemy2;
+
+    /// <summary>
+    /// 開始事件
+    /// </summary>
+    public void Start()
+    {
+        //取得
+        print(enemy1.mp);               //怪物1號的魔力
+        print(enemy2.equipment);        //怪物2號的裝備名稱
+
+        //設定
+        enemy1.hp = 100;                //怪物1號的血量
+        enemy2.hp = 300;                //怪物2號的血量
+        enemy1.defense = 50;            //怪物1號的防禦力
+        enemy1.weapon = "木棍";         //怪物1號的武器
+        enemy2.weapon = "短刀";         //怪物2號的武器
+        enemy1.key = true;              //怪物1號帶有鑰匙
+        enemy2.treasure = true;         //怪物2號帶有寶物
+    }
+}
